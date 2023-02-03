ea78ce2da1be213ffdd32342740fcd5656fb52bc
Tsoko Malusi <tsokomalusi@outlook.com>
2023-02-03 20:51:49 +0200

    "indented the code"

--- src/test/Day1.java
+++ src/test/Day1.java
@@ -10,20 +10,21 @@ public class Day1 {
 	public static void main(String[] args)  {
 
 		WebDriver driver;
-		
+
 		//AUT
 		String baseURL = "https://www.demo.guru99.com/V4/";
 
+		//Instantiating the driver
 		driver = new FirefoxDriver();
-		
+
 		//Launching browser session
 		driver.get(baseURL);
-		
+
 		//Locating GUI elements
-        driver.findElement(By.name("uid")).sendKeys("mngr471494");
+		driver.findElement(By.name("uid")).sendKeys("mngr471494");
 		driver.findElement(By.name("password")).sendKeys("yzuqAma");
 		driver.findElement(By.name("btnLogin")).click();
-		
+
 		//Terminating browser session
 		driver.quit();
 
