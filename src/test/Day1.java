b088db875927052a3520e4c51d6e48d48263ecdb
Tsoko Malusi <tsokomalusi@outlook.com>
2023-02-04 11:15:39 +0200

    "added firefox binary path"

--- src/test/Day1.java
+++ src/test/Day1.java
@@ -7,12 +7,15 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class Day1 {
 
 
-	public static void main(String[] args)  {
+	public static void main(String[] args) {
 
 		WebDriver driver;
 
 		//AUT
 		String baseURL = "https://www.demo.guru99.com/V4/";
+
+		System.setProperty("webdriver.gecko.driver", "C:\\Drivers\\geckodriver.exe");
+		System.setProperty("webdriver.firefox.bin",  "C:\\Program Files\\Mozilla Firefox\\firefox");
 
 		//Instantiating the driver
 		driver = new FirefoxDriver();
@@ -21,15 +24,12 @@ public class Day1 {
 		driver.get(baseURL);
 
 		//Locating GUI elements
-		driver.findElement(By.name("uid")).sendKeys("mngr471494");
-		driver.findElement(By.name("password")).sendKeys("yzuqAma");
+		driver.findElement(By.name("uid")).sendKeys("mngr476324");
+		driver.findElement(By.name("password")).sendKeys("supYsYb");
 		driver.findElement(By.name("btnLogin")).click();
 
 		//Terminating browser session
 		driver.quit();
-
-
-
 	}
 
 }
