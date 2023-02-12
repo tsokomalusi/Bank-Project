59c45078b1a2381f2181b488efe111385a4c247d
Tsoko Malusi <tsokomalusi@outlook.com>
2023-02-12 22:12:24 +0200

    "added screenshot for successful login"

--- src/test/FinalScript.java
+++ src/test/FinalScript.java
@@ -5,22 +5,29 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
+
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
 import org.testng.annotations.AfterTest;
 
-public class TestScript05 {
+public class FinalScript {
 
 	public WebDriver driver;
 	public String baseUrl;
-	
-	
-      
+
+
+
 	/*
 	 * data set of username and password for testing
 	 */
@@ -64,7 +71,7 @@ public class TestScript05 {
 					(By.xpath(path)).getText();
 
 			System.out.println(text);
-			
+
 			// Extract the dynamic text mngrXXXX on page		
 			String[] parts = text.split(Util.PATTERN);
 			String dynamicText = parts[1];
@@ -72,14 +79,28 @@ public class TestScript05 {
 			// Check that the dynamic text is of pattern mngrXXXX
 			// First 4 characters must be "mngr"
 			assertTrue(dynamicText.substring(1, 5).equals(Util.FIRST_PATTERN));
-			
+
 			// remain stores the "XXXX" in pattern mngrXXXX
 			String remain = dynamicText.substring(dynamicText.length() - 4);
-			
+
 			// Check remain string must be numbers;
 			assertTrue(remain.matches(Util.SECOND_PATTERN));
-			
-			
+
+			//code for taking screenshot
+			TakesScreenshot ts = (TakesScreenshot)driver;
+
+			//capture screenshot as output type FILE
+			File file = ts.getScreenshotAs(OutputType.FILE);
+
+			try {
+				//save the screenshot taken in destination path
+				FileUtils.copyFile(file, new File("./resources/screenshots/validLoginop.png"));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			System.out.println("the login page screenshot is taken");
+
+
 		}
 
 		else {
