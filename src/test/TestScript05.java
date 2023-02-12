92a6241712aa8a1f491c5473066c0c4f283e1923
Tsoko Malusi <tsokomalusi@outlook.com>
2023-02-12 14:10:32 +0200

    "Verifying login functionality with dataProvider"

---
+++ src/test/TestScript05.java
@@ -0,0 +1,113 @@
+package test;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+import java.time.Duration;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.annotations.AfterTest;
+
+public class TestScript05 {
+
+	public WebDriver driver;
+	public String baseUrl;
+
+	/*
+	 * data set of username and password for testing
+	 */
+
+	@DataProvider(name = "credentials")
+	public Object[][] testData()  {
+
+		return new Object[][] {
+
+			{"bothvalid", Util.USER_NAME, Util.PASSWD},
+			{"validpassword", "mngr0020223", Util.PASSWD},
+			{"validusername", Util.USER_NAME, "supXsXb"},
+			{"bothinvalid", "mngr002022", "supXsXb"}
+
+		};
+	}
+
+	@Test(dataProvider = "credentials" )
+	public void testCase05(String scenario, String username, 
+			String password) throws Exception {
+
+		String actualBoxMsg;
+
+		//Enter username
+		driver.findElement(By.name("uid")).clear();
+		driver.findElement(By.name("uid")).sendKeys(username);
+
+		//Enter password
+		driver.findElement(By.name("password")).clear();
+		driver.findElement(By.name("password")).sendKeys(password);
+
+		// Click Login
+		driver.findElement(By.name("btnLogin")).click();
+
+		if(scenario.equals("bothvalid"))  {
+
+			// Get text displayes on login page
+
+			String path = "/html[1]/body[1]/table[1]/tbody[1]/tr[1]/td[1]/table[1]/tbody[1]/tr[3]/td[1]";
+			String text = driver.findElement
+					(By.xpath(path)).getText();
+
+			System.out.println(text);
+			
+			// Extract the dynamic text mngrXXXX on page		
+			String[] parts = text.split(Util.PATTERN);
+			String dynamicText = parts[1];
+
+			// Check that the dynamic text is of pattern mngrXXXX
+			// First 4 characters must be "mngr"
+			assertTrue(dynamicText.substring(1, 5).equals(Util.FIRST_PATTERN));
+			
+			// remain stores the "XXXX" in pattern mngrXXXX
+			String remain = dynamicText.substring(dynamicText.length() - 4);
+			
+			// Check remain string must be numbers;
+			assertTrue(remain.matches(Util.SECOND_PATTERN));
+
+		}   
+
+		else {
+
+			Alert alt = driver.switchTo().alert();
+			actualBoxMsg = alt.getText(); // get content of the Alter Message
+			alt.accept();
+			// Compare Error Text with Expected Error Value					
+			assertEquals(actualBoxMsg,Util.EXPECT_ERROR);
+		}
+
+
+	}
+	@BeforeTest
+	public void setUp() throws Exception  {
+
+		FirefoxOptions options = new FirefoxOptions();
+
+		driver = new FirefoxDriver(options);
+
+		baseUrl = Util.BASE_URL;
+
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(Util.WAIT_TIME));
+
+		driver.get(baseUrl);
+
+	}
+
+	@AfterTest
+	public void afterTest() throws Exception {
+
+		driver.quit();
+	}
+
+}
--- src/test/Util.java
+++ src/test/Util.java
@@ -1,20 +1,8 @@
 package test;
 
-import java.io.File;
-import java.io.FileInputStream;
-
-import java.io.IOException;
-import org.apache.poi.ss.usermodel.DataFormatter;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.usermodel.WorkbookFactory;
-
 
 public  class Util {
 
-	//public static final String FILE_PATH = "data.xlsx";
-	public static final String SHEET_NAME = "Sheet1"; 
 	public static final int WAIT_TIME = 30;
 	public static final String BASE_URL = "http://www.demo.guru99.com/v4/";
 	public static final String EXPECT_TITLE = "Guru99 Bank Manager HomePage";
@@ -24,44 +12,9 @@ public  class Util {
 	public static final String USER_NAME = "mngr476324";
 	public static final String PASSWD = "supYsYb";
 	
-	
+	 public static final String PATTERN = ":";
+	 public static final String FIRST_PATTERN = "mngr";
+	public static final String SECOND_PATTERN = "[0-9]+";
 
-	public static String[][] getXLData(String xlSheetName) throws IOException 
-	{
-     
-		File file = new File(System.getProperty("user.dir") + "\\excel\\data.xlsx");
-
-		FileInputStream fis = new FileInputStream(file);
-
-		Workbook wb = WorkbookFactory.create(fis);
-
-		Sheet sheetName = wb.getSheet(xlSheetName);
-
-		int totalRows = sheetName.getLastRowNum();
-
-		//System.out.println(totalRows);
-
-		Row rowCells = sheetName.getRow(0);
-
-		int totalCols = rowCells.getLastCellNum();
-
-		//System.out.println(totalCols);
-		
-		DataFormatter format = new DataFormatter();
-		
-		String testData[][] = new String[totalRows][totalCols];
-		
-		for(int i = 1; i <= totalRows; i++)  {
-			
-			for(int j = 0; j < totalCols; j++) {
-				
-			    testData[i-1][j] = format.formatCellValue(sheetName.getRow(i).getCell(j));
-			    
-			   // System.out.println(testData[i-1][j]);
-			}
-		}
-
-             return testData;
-	}	 
 
 }
